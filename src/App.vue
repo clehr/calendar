9415e9f73cb281ca3606309dfc9c33e737d0ffa6
Christian Lehr <christian_lehr.1@gmx.de>
2019-05-17 01:27:08 +0200

    add entries

--- src/App.vue
+++ src/App.vue
@@ -1,22 +1,26 @@
 <template>
     <div id="app" v-if="true">
-        Gib dein Passwort ein:
+        Passwort:
         <input type="password" v-model="password">
-        {{this.firebasepassword}}
+
+        <div v-bind:key="entry" v-for="entry in entries">{{entry}}</div>
+
+        <div>
+            <textarea  v-model="entry" @keyup.enter="addEntry" />
+        </div>
 
         <div v-if="password == 'test'">
-            <ul>
-                <li><input></li>
-                <li><input></li>
-                <li><input></li>
-            </ul>
+            hidden section
         </div>
-        <button @click="savePassword">Save</button>
+        <button @click="saveEntries">Save</button>
     </div>
 </template>
 
 <script>
     import {db} from './config/db';
+    import * as firebase from "firebase";
+
+    const database = firebase.database();
 
     export default {
         name: 'app',
@@ -26,14 +30,19 @@
         },
         data: function () {
             return {
-                password: ''
+                password: '',
+                entries: [],
+                entry: ''
             };
         },
         methods: {
-            savePassword() {
-                this.$firebaseRefs.firebasepassword.push({
-                    firebasepassword: this.password
+            saveEntries() {
+                this.$firebaseRefs.entries.push({
+                    entries: this.entries
                 });
+            },
+            addEntry() {
+                this.entries.push(this.entry);
             }
 
         }
