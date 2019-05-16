22ab2fd59da2f6b1501b71716ce5a7a9a38e806f
Christian Lehr <christian_lehr.1@gmx.de>
2019-05-16 21:40:50 +0200

    add input field

--- src/App.vue
+++ src/App.vue
@@ -1,7 +1,7 @@
 <template>
   <div id="app">
     <ul>
-      <li>19.5.2019 Termin beim Arzt um 9 Uhr im Hafestuder Weg 23</li>
+      <li>19.5.2019 <input /> um 9 Uhr im Hafestuder Weg 23</li>
       <li>14.5.2019 Treffen mit Steffi um 9 Uhr im Macumba</li>
       <li>20.11.2019 Geburtstag planen um 15:30-17:30 Uhr</li>
     </ul>
